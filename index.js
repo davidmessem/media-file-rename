267727c156d5948dfc7b0ce5437858b88c467091
David Messem <megalodon@megalodon.local>
2020-05-31 13:33:05 +0200

    initial

---
+++ index.js
@@ -0,0 +1,69 @@
+const fs = require('fs');
+const path = require('path');
+const { getVideoDurationInSeconds } = require('get-video-duration')
+
+const fileExtension = process.argv[2]
+const prepend = process.argv[3]
+const directoryPath = process.argv[4]
+
+if (!fileExtension || !prepend || !path) {
+  console.error("Usage: node index.js <file extension> <prepend> <path>");
+  console.error("Example: node index.js mp4 bike_ride ~/videos");
+  process.exit(1);
+}
+
+fs.promises
+  .readdir(directoryPath, { withFileTypes: true })
+  .then(directoryEntries =>
+    directoryEntries
+      .filter(entry => entry.isFile())
+      .filter(entry => entry.name.toLowerCase().endsWith(fileExtension.toLowerCase()))
+      .map(entry => entry.name)
+  )
+  .then(fileNames =>
+    Promise.all(
+      fileNames.map(fileName =>
+        fs.promises.stat(directoryPath + "/" + fileName)
+          .then(stats => {
+            return {
+              name: fileName,
+              path: directoryPath,
+              birthTime: stats.birthtime,
+              extension: path.extname(fileName)
+            }
+          })
+      )
+    )
+  )
+  .then(filesStatInfo =>
+    Promise.all(filesStatInfo.map(fileStatInfo =>
+      getVideoDurationInSeconds(fileStatInfo.path + "/" + fileStatInfo.name)
+        .then(durationSeconds => {
+          return {
+            ...fileStatInfo,
+            duration: secondsToString(durationSeconds)
+          }
+        })
+    ))
+  )
+  .then(videosInfo => {
+    console.log("videosInfo", videosInfo)
+    return Promise.all(
+      videosInfo.map(videoInfo => {
+        const dateTime = videoInfo.birthTime.toISOString().substring(0, 16).replace('T', '__').replace(':', 'H')
+        const newName = `${prepend}___${dateTime}___${videoInfo.duration}${videoInfo.extension}`
+        console.log(`${videoInfo.name} -> ${newName}`)
+        return fs.promises.rename(`${videoInfo.path}/${videoInfo.name}`, `${videoInfo.path}/${newName}`)
+      })
+    )
+  })
+  .then(() => console.log("Done"))
+
+function secondsToString(seconds) {
+  var numHours = Math.floor(((seconds % 31536000) % 86400) / 3600);
+  var numMinutes = Math.floor((((seconds % 31536000) % 86400) % 3600) / 60);
+  var numSeconds = Math.floor((((seconds % 31536000) % 86400) % 3600) % 60);
+  return (numHours ? numHours + "h" : "")
+    + (numMinutes ? numMinutes + "m" : "")
+    + (numSeconds ? numSeconds + "s" : "");
+}
